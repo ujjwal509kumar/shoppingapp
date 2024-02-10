790316e98b1eed909c83c69c62ed6748e9a6966e
Lokesh <lokeshlokey32@gmail.com>
2024-02-10 20:17:30 +0530

    index page
    index page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,3 @@
-import Header from '../components/Header'
-import Footer from '../components/Footer'
 import TextRecycler from '../components/TextRecycler'
 import RightArrow from '../components/RightArrow'
 import ProductSection from '../components/ProductSection'
