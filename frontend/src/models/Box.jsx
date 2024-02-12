25f907b40297a10cabb8403fb87f358514cb3955
Ujjwal Kumar <ujjwal509kumar@gmail.com>
2024-02-12 22:59:34 +0530

    fixed indentation

--- frontend/src/models/Box.jsx
+++ frontend/src/models/Box.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import { useRef, useEffect } from "react";
 import * as THREE from "three";
 import ThreeGlobe from "three-globe";
 import data from "./data/custom.geo.json";
