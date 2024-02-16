d024dd437844513db8bc37473e5efda9b2c4b313
Lokesh.S <121326276+Lokesh-Repository@users.noreply.github.com>
2024-02-16 15:37:13 +0530

    Logo Change
    Logo Change

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -23,10 +23,7 @@ const Header = () => {
     <div>
       <div className="flex flex-col px-10 py-5 md:flex-row md:justify-start justify-center items-center shadow-md sticky top-0 bg-white z-10">
         <a href="" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-          </svg>
-          <span className="ml-3 text-xl">Shop360</span>
+        <img className="w-auto h-20 rounded" src="../src/assets/logo.png" alt=""></img>
         </a>
         <div className="nav">
           <ul className="flex items-center space-x-2 font-bold md:text-xl">
