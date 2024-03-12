27f244103a695dcdc13996061b8e59e18fdd6555
Lokesh.S <121326276+Lokesh-Repository@users.noreply.github.com>
2024-03-12 21:41:46 +0530

    Update TrackOrder.jsx

--- frontend/src/pages/TrackOrder.jsx
+++ frontend/src/pages/TrackOrder.jsx
@@ -34,7 +34,7 @@ return (
 
 
   
-<div className="py-14 px-4 md:px-6 2xl:px-20 2xl:container 2xl:mx-auto bg-blue-100 rounded-lg" hidden>
+<div className="py-14 px-4 md:px-6 2xl:px-20 2xl:container 2xl:mx-auto bg-blue-100 rounded-lg">
   <div className="flex justify-start item-start space-y-2 flex-col">
     <h1 className="text-3xl lg:text-4xl font-semibold leading-7 lg:leading-9 text-gray-800">Order #123456</h1>
     <p className="text-base font-medium leading-6 text-gray-600">21st Mart 2021 at 10:34 PM</p>
@@ -58,9 +58,9 @@ return (
               </div>
             </div>
             <div className="flex justify-between space-x-8 items-start w-full">
-              <p className="text-base dark:text-white xl:text-lg leading-6">$36.00 <span className="text-red-300 line-through"> $45.00</span></p>
+              <p className="text-base dark:text-white xl:text-lg leading-6"> ₹36.00 <span className="text-red-300 line-through">  ₹45.00</span></p>
               <p className="text-base dark:text-white xl:text-lg leading-6 text-gray-800">01</p>
-              <p className="text-base dark:text-white xl:text-lg font-semibold leading-6 text-gray-800">$36.00</p>
+              <p className="text-base dark:text-white xl:text-lg font-semibold leading-6 text-gray-800"> ₹36.00</p>
             </div>
           </div>
         </div>
@@ -79,9 +79,9 @@ return (
               </div>
             </div>
             <div className="flex justify-between space-x-8 items-start w-full">
-              <p className="text-base dark:text-white xl:text-lg leading-6">$20.00 <span className="text-red-300 line-through"> $30.00</span></p>
+              <p className="text-base dark:text-white xl:text-lg leading-6"> ₹20.00 <span className="text-red-300 line-through">  ₹30.00</span></p>
               <p className="text-base dark:text-white xl:text-lg leading-6 text-gray-800">01</p>
-              <p className="text-base dark:text-white xl:text-lg font-semibold leading-6 text-gray-800">$20.00</p>
+              <p className="text-base dark:text-white xl:text-lg font-semibold leading-6 text-gray-800"> ₹20.00</p>
             </div>
           </div>
         </div>
@@ -92,20 +92,20 @@ return (
           <div className="flex justify-center items-center w-full space-y-4 flex-col border-gray-200 border-b pb-4">
             <div className="flex justify-between w-full">
               <p className="text-base dark:text-white leading-4 text-gray-800">Subtotal</p>
-              <p className="text-base dark:text-gray-300 leading-4 text-gray-600">$56.00</p>
+              <p className="text-base dark:text-gray-300 leading-4 text-gray-600"> ₹56.00</p>
             </div>
             <div className="flex justify-between items-center w-full">
               <p className="text-base dark:text-white leading-4 text-gray-800">Discount <span className="bg-green-200 rounded p-1 text-xs font-medium leading-3 text-black">STUDENT</span></p>
-              <p className="text-base dark:text-gray-300 leading-4 text-gray-600">-$28.00 (50%)</p>
+              <p className="text-base dark:text-gray-300 leading-4 text-gray-600">- ₹28.00 (50%)</p>
             </div>
             <div className="flex justify-between items-center w-full">
               <p className="text-base dark:text-white leading-4 text-gray-800">Shipping</p>
-              <p className="text-base dark:text-gray-300 leading-4 text-gray-600">$8.00</p>
+              <p className="text-base dark:text-gray-300 leading-4 text-gray-600"> ₹8.00</p>
             </div>
           </div>
           <div className="flex justify-between items-center w-full">
             <p className="text-base dark:text-white font-semibold leading-4 text-gray-800">Total</p>
-            <p className="text-base dark:text-gray-300 font-semibold leading-4 text-gray-600">$36.00</p>
+            <p className="text-base dark:text-gray-300 font-semibold leading-4 text-gray-600"> ₹36.00</p>
           </div>
         </div>
         <div className="flex flex-col justify-center px-4 py-6 md:p-6 xl:p-8 w-full bg-blue-500 rounded-lg space-y-6">
@@ -119,7 +119,7 @@ return (
                 <p className="text-lg leading-6 dark:text-white font-semibold text-gray-800">DPD Delivery<br /><span className="font-normal">Delivery with 24 Hours</span></p>
               </div>
             </div>
-            <p className="text-lg font-semibold leading-6 dark:text-white text-gray-800">$8.00</p>
+            <p className="text-lg font-semibold leading-6 dark:text-white text-gray-800"> ₹8.00</p>
           </div>
           <div className="w-full flex justify-center items-center">
             <button className="hover:bg-black dark:bg-white dark:text-gray-800 dark:hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 py-5 w-96 md:w-full bg-gray-800 text-base font-medium leading-4 text-white">View Carrier Details</button>
@@ -134,7 +134,7 @@ return (
           <div className="flex justify-center w-full md:justify-start items-center space-x-4 py-8 border-b border-gray-200">
             <img src="https://i.ibb.co/5TSg7f6/Rectangle-18.png" alt="avatar" />
             <div className="flex justify-start items-start flex-col space-y-2">
-              <p className="text-base dark:text-white font-semibold leading-4 text-left text-gray-800">David Kent</p>
+              <p className="text-base dark:text-white font-semibold leading-4 text-left text-gray-800">Chiru Bhai</p>
               <p className="text-sm dark:text-gray-300 leading-5 text-gray-600">10 Previous Orders</p>
             </div>
           </div>
@@ -144,21 +144,22 @@ return (
               <path d="M19 5H5C3.89543 5 3 5.89543 3 7V17C3 18.1046 3.89543 19 5 19H19C20.1046 19 21 18.1046 21 17V7C21 5.89543 20.1046 5 19 5Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
               <path d="M3 7L12 13L21 7" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
             </svg>
-            <p className="cursor-pointer text-sm leading-5 ">david89@gmail.com</p>
+            <p className="cursor-pointer text-sm leading-5 ">chirubhai@gmail.com</p>
           </div>
         </div>
         <div className="flex justify-between xl:h-full items-stretch w-full flex-col mt-6 md:mt-0">
           <div className="flex justify-center md:justify-start xl:flex-col flex-col md:space-x-6 lg:space-x-8 xl:space-x-0 space-y-4 xl:space-y-12 md:space-y-0 md:flex-row items-center md:items-start">
             <div className="flex justify-center md:justify-start items-center md:items-start flex-col space-y-4 xl:mt-8">
               <p className="text-base dark:text-white font-semibold leading-4 text-center md:text-left text-gray-800">Shipping Address</p>
-              <p className="w-48 lg:w-full dark:text-gray-300 xl:w-48 text-center md:text-left text-sm leading-5 text-gray-600">180 North King Street, Northhampton MA 1060</p>
+              <p className="w-48 lg:w-full dark:text-gray-300 xl:w-48 capitalize text-center md:text-left text-sm leading-5 text-gray-600">dont know your address bro,kadirenhalli,Bangalore 560059</p>
             </div>
             <div className="flex justify-center md:justify-start items-center md:items-start flex-col space-y-4">
               <p className="text-base dark:text-white font-semibold leading-4 text-center md:text-left text-gray-800">Billing Address</p>
-              <p className="w-48 lg:w-full dark:text-gray-300 xl:w-48 text-center md:text-left text-sm leading-5 text-gray-600">180 North King Street, Northhampton MA 1060</p>
+              <p className="w-48 lg:w-full dark:text-gray-300 xl:w-48 capitalize text-center md:text-left text-sm leading-5 text-gray-600">dont know your address bro,kadirenhalli,Bangalore 560059</p>
             </div>
           </div>
-          <div className="flex w-full justify-center items-center md:justify-start md:items-start">
+          <div className="flex flex-col justify-center items-center">
+           <a href="/TrackOrder"><button className="mt-6 md:mt-0 dark:border-white dark:hover:bg-gray-900 dark:bg-transparent dark:text-white py-5 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 border border-gray-800 font-medium w-96 2xl:w-full text-base font-medium leading-4 text-gray-800">Check Another</button></a>
             <button className="mt-6 md:mt-0 dark:border-white dark:hover:bg-gray-900 dark:bg-transparent dark:text-white py-5 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 border border-gray-800 font-medium w-96 2xl:w-full text-base font-medium leading-4 text-gray-800">Edit Details</button>
           </div>
         </div>
